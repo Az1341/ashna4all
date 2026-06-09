ae30cd210a82a040960f0a33b8e2cfd2f17ce57a
Az1341 <122234469+Az1341@users.noreply.github.com>
2026-06-09 20:14:12 +0100

    Filter World Cup matches by competition code

--- api/scores.js
+++ api/scores.js
@@ -7,9 +7,11 @@ export default async function handler(req, res) {
 
   var date    = req.query.date || new Date().toISOString().slice(0,10);
   var matchId = req.query.id   || null;
-  var url     = matchId
+
+  /* Always filter by WC competition code for World Cup matches */
+  var url = matchId
     ? 'https://api.football-data.org/v4/matches/'+matchId
-    : 'https://api.football-data.org/v4/matches?dateFrom='+date+'&dateTo='+date;
+    : 'https://api.football-data.org/v4/competitions/WC/matches?dateFrom='+date+'&dateTo='+date;
 
   try {
     var r    = await fetch(url, {headers:{'X-Auth-Token':key}});
